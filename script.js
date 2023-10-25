6168dac457699fab0a18bbceb7fe5e50021ddb84
Ivan <127977316+ivan-developer-01@users.noreply.github.com>
2023-10-25 15:38:05 +0300

    add geolocation button to the chat

--- script.js
+++ script.js
@@ -1,11 +1,66 @@
 const wsURL = "wss://echo-ws-service.herokuapp.com/";
 const inputForm = document.querySelector(".input-wrapper form"),
+	submitButton = document.querySelector(".input-wrapper form button.submit"),
+	geolocationButton = document.querySelector(
+		".input-wrapper form button.geolocation"
+	),
 	input = document.querySelector(".input"),
 	messages = document.querySelector(".messages");
 
+const actions = {
+	NONE: "NONE",
+	START: "START",
+	SEND_USUAL: "SEND_USUAL",
+	SEND_GEOLOCATION: "SEND_GEOLOCATION",
+	RECEIVE: "RECEIVE",
+	APPEND_MESSAGE: "APPEND_MESSAGE",
+};
+
+let lastAction = actions.NONE;
+
 const websocket = new WebSocket(wsURL);
+lastAction = actions.START;
 
 inputForm.addEventListener("submit", (event) => {
+	event.preventDefault();
+});
+
+submitButton.addEventListener("click", sendUsual);
+geolocationButton.addEventListener("click", () => {
+	if ("geolocation" in navigator) {
+		navigator.geolocation.getCurrentPosition((position) => {
+			lastAction = actions.SEND_GEOLOCATION;
+			const data = {
+				latitude: position.coords.latitude,
+				longitude: position.coords.longitude,
+			};
+
+			websocket.send(JSON.stringify(data));
+			appendMessage(
+				`<a href="https://www.openstreetmap.org/#map=18/${data.latitude}/${data.longitude}">Гео-локация</a>`,
+				"mine",
+				true
+			);
+		});
+	}
+});
+
+input.addEventListener("keypress", (event) => {
+	if (event.key === "Enter") {
+		sendUsual(event);
+	}
+});
+
+websocket.addEventListener("message", (event) => {
+	if (lastAction === actions.SEND_USUAL) {
+		appendMessage(event.data, "system");
+	}
+
+	lastAction = actions.RECEIVE;
+});
+
+function sendUsual(event) {
+	lastAction = actions.SEND_USUAL;
 	event.preventDefault();
 	const message = input.value;
 	if (!message) return;
@@ -13,15 +68,16 @@ inputForm.addEventListener("submit", (event) => {
 	appendMessage(message, "mine");
 	websocket.send(message);
 	input.value = "";
-});
+}
 
-websocket.addEventListener("message", (event) => {
-	appendMessage(event.data, "system");
-});
-
-function appendMessage(message, type) {
+function appendMessage(message, type, useInnerHTML = false) {
 	let messageBlock = document.createElement("div");
 	messageBlock.classList.add("message", type);
-	messageBlock.textContent = message;
+	if (useInnerHTML) {
+		messageBlock.innerHTML = message;
+	} else {
+		messageBlock.textContent = message;
+	}
+
 	messages.appendChild(messageBlock);
 }
